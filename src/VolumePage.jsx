8bf0873fc724b706151b571b087d01c0013cbae2
hoang0508 <nguyenhuyhoang05082001@gmail.com>
2025-04-28 07:52:50 +0700

    h27

--- src/VolumePage.jsx
+++ src/VolumePage.jsx
@@ -5,7 +5,7 @@ import { Volume2, VolumeOff } from "lucide-react";
 const VolumePage = ({ song }) => {
   const { isPlaying, togglePlay } = useAudioPlayer(song);
   return (
-    <button className="fixed bottom-5 right-[30px]" onClick={togglePlay}>
+    <button className="fixed bottom-3 right-[30px]" onClick={togglePlay}>
       {" "}
       {isPlaying ? (
         <Volume2 size={40} strokeWidth={1.75} />
